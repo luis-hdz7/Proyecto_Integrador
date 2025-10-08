1b26f2fa447cc8c62d0199dc0d30d147ce6858da
Luis Hernández <luisjorgeantonhernandez@gmail.com>
2025-10-08 10:34:52 -0600

    Comentando logging.py

--- logging.py
+++ logging.py
@@ -12,7 +12,8 @@ def logging():
     errores = []
 
     email = input("Email: ")
-    patron_email = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+    patron_email = r'^[\w\.-]+@[\w\.-]+\.\w+$'#Esta expresión regular verifica que la cadena tenga una estructura básica del tipo usuario@dominio.extension
+    if not re.match(patron_email, email):
     if not re.match(patron_email, email):
         errores.append("Email no válido, no cumple con el formato válido.")
 
@@ -28,4 +29,5 @@ def logging():
         return logging()
     else:
         print("✅ Datos procesados correctamente.\n")
+
         return email, password
